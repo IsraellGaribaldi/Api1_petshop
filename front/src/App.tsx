222a23e9e5b7133a01081853c2c08cac1d6488af
Vinicius Emanuel <viniesn9@gmail.com>
2025-11-25 15:43:58 -0300

    alterações home
    mudança da logo e outras coisas

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,15 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
 import PetsPage from "./pages/PetsPage";
+import Home from "./pages/Home";
 
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/pets" element={<PetsPage />} />
     </Routes>
