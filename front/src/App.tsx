7eb89c01480c7dde1ece4b2ceae29527c3e402d9
Vinicius Emanuel <viniesn9@gmail.com>
2025-11-25 14:23:45 -0300

    mudei o app.tsx
    fds

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PetsPage from "./pages/PetsPage";
 
 function App() {
@@ -9,8 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/pets" element={<PetsPage />} />
-        <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
