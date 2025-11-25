dabce78c752ce1ecf0936884a7cceee896bfd352
Ana <anab.costac@gmail.com>
2025-11-25 15:22:11 -0300

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/pets" element={<PetsPage />} />
+      <Route path="*" element={<div>Página não encontrada</div>} />
     </Routes>
   );
 }
