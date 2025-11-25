0fd8f403e1eb68ddfacc1d3e20d0bbab4b8be745
Ana <anab.costac@gmail.com>
2025-11-25 14:51:09 -0300

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import PetsPage from "./pages/PetsPage";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/pets" element={<PetsPage />} />
-      <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
