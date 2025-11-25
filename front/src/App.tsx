efc3b1f5656b7789c159d450effa21e67fb2d2ca
Ana <anab.costac@gmail.com>
2025-11-25 15:39:02 -0300

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -6,13 +6,11 @@ import PetsPage from "./pages/PetsPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/pets" element={<PetsPage />} />
-        <Route path="*" element={<div>Página não encontrada</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/pets" element={<PetsPage />} />
+      <Route path="*" element={<div>Página não encontrada</div>} />
+    </Routes>
   );
 }
